4a1f1b2b146f41604cf3daf78dc74b179e055426
unknown <jalovatto@hotmail.com>
2020-08-20 19:26:36 -0300

    pock it busca

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,13 +1,22 @@
 console.log('start buscarpock');
 
 function buscarpock(){
+	let txtopcion = document.getElementById('opcion').value;
+	
+	let img = document.getElementById('img');
+	let etiqueta = document.getElementById('datos');
 	let xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/1");
+	xhttp.open("GET", 'https://pokeapi.co/api/v2/pokemon/${txtopcion}');
 	xhttp.send();
 	
 	xhttp.onreadystatechange=function(){
 		if(this.readyState == 4 && this.status == 200){
-			console.log( JSON.parse(this.response) );
+			let datosp =  JSON.parse(this.response);
+			console.log( datosp );
+			
+			img.setAttribute("src",datosp.sprites.front_default);
+			etiqueta.value = datosp.name;
+			
 		}
 
 	}
